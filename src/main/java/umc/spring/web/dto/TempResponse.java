adfaf0099f20e22539ff634fd63bd39e81eb61eb
Sohyunnnn <thgus05061@naver.com>
2025-05-14 18:44:42 +0900

    feat: 임시 파일 생성

---
+++ src/main/java/umc/spring/web/dto/TempResponse.java
@@ -0,0 +1,14 @@
+package umc.spring.web.dto;
+
+import lombok.*;
+
+public class TempResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempTestDTO{
+        String testString;
+    }
+}
