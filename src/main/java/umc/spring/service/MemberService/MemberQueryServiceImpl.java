02072b29c87112546ad4c3e0e92ad8db473200d0
Sohyunnnn <thgus05061@naver.com>
2025-05-21 11:06:47 +0900

    feat: 멤버 생성 기능 추가

--- src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
@@ -1,4 +1,20 @@
 package umc.spring.service.MemberService;
 
-public class MemberQueryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.spring.domain.mapping.Member;
+import umc.spring.repository.MemberRepository;
+import umc.spring.web.dto.MemberRequestDTO;
+
+@Service
+@RequiredArgsConstructor
+public class MemberCommandServiceImpl implements MemberCommandService{
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member joinMember(MemberRequestDTO.JoinDto request) {
+
+        return null;
+    }
 }
