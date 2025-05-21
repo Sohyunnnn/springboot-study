eb730a9cd11b114fea3c532ce391f824992d93d5
Sohyunnnn <thgus05061@naver.com>
2025-05-21 18:49:23 +0900

    feat: error state 추가

--- src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -23,7 +23,11 @@ public enum ErrorStatus implements BaseErrorCode {
     // 예시,,,
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
 
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD4001", "음식 카테고리를 찾을 수 없습니다."),
+
+
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
+
 
     private final HttpStatus httpStatus;
     private final String code;
