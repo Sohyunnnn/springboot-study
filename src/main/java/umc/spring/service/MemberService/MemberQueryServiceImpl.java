c0b6105dca03cee84ee345088f33e127efef9914
Sohyunnnn <thgus05061@naver.com>
2025-05-21 11:17:34 +0900

    refactor: 코드 구조 변경

--- src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryServiceImpl.java
@@ -1,20 +1,4 @@
 package umc.spring.service.MemberService;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import umc.spring.domain.mapping.Member;
-import umc.spring.repository.MemberRepository;
-import umc.spring.web.dto.MemberRequestDTO;
-
-@Service
-@RequiredArgsConstructor
-public class MemberCommandServiceImpl implements MemberCommandService{
-
-    private final MemberRepository memberRepository;
-
-    @Override
-    public Member joinMember(MemberRequestDTO.JoinDto request) {
-
-        return null;
-    }
+public class MemberQueryServiceImpl {
 }
