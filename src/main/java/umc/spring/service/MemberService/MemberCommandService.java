9c15e7d0a0967992d834e2d3881cee2764ef777a
Sohyunnnn <thgus05061@naver.com>
2025-06-04 19:08:46 +0900

    feat: 로그인 로직 구현

--- src/main/java/umc/spring/service/MemberService/MemberCommandService.java
+++ src/main/java/umc/spring/service/MemberService/MemberCommandService.java
@@ -2,8 +2,11 @@ package umc.spring.service.MemberService;
 
 import umc.spring.domain.mapping.Member;
 import umc.spring.web.dto.MemberRequestDTO;
+import umc.spring.web.dto.MemberResponseDTO;
 
 public interface MemberCommandService {
 
     Member joinMember(MemberRequestDTO.JoinDto request);
+    MemberResponseDTO.LoginResultDTO loginMember(MemberRequestDTO.LoginRequestDTO request);
+
 }
