7fb87a11275d3c46b106f294e683bd7552e70079
Sohyunnnn <thgus05061@naver.com>
2025-06-04 18:59:58 +0900

    feat: 로그인 요청 및 응답 dto 정의

--- src/main/java/umc/spring/web/dto/MemberResponseDTO.java
+++ src/main/java/umc/spring/web/dto/MemberResponseDTO.java
@@ -14,4 +14,13 @@ public class MemberResponseDTO {
         Long memberId;
         LocalDateTime createdAt;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LoginResultDTO {
+        Long memberId;
+        String accessToken;
+    }
 }
