51ab7bc1dfa72abbe40af31926f1e085cf41dddf
Sohyun Park <108279922+Sohyunnnn@users.noreply.github.com>
2025-06-06 18:07:45 +0900

    [Feature/#10] 토큰 기반 로그인 구현 (#11)
    * chore: 의존성 추가
    
    * feat: 로그인 요청 및 응답 dto 정의
    
    * feat: 전역 클래스 정의
    
    * feat: jwt 설정 추가
    
    * feat: 인증 허용 경로 추가
    
    * feat: 로그인 로직 구현
    
    * feat: 로그인 로직 구현 및 에러 처리
    
    * feat: service 로직 추가

--- src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
+++ src/main/java/umc/spring/service/MemberService/MemberCommandServiceImpl.java
@@ -2,10 +2,14 @@ package umc.spring.service.MemberService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.FoodCategoryHandler;
+import umc.spring.apiPayload.exception.handler.MemberHandler;
+import umc.spring.config.security.jwt.JwtTokenProvider;
 import umc.spring.converter.MemberConverter;
 import umc.spring.converter.MemberPreferConverter;
 import umc.spring.domain.mapping.FoodCategory;
@@ -14,7 +18,9 @@ import umc.spring.domain.mapping.MemberPrefer;
 import umc.spring.repository.FoodCategoryRepository;
 import umc.spring.repository.MemberRepository;
 import umc.spring.web.dto.MemberRequestDTO;
+import umc.spring.web.dto.MemberResponseDTO;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,6 +33,8 @@ public class MemberCommandServiceImpl implements MemberCommandService{
     private final FoodCategoryRepository foodCategoryRepository;
 
     private final PasswordEncoder passwordEncoder;
+
+    private final JwtTokenProvider jwtTokenProvider;
 
 
     @Override
@@ -47,5 +55,27 @@ public class MemberCommandServiceImpl implements MemberCommandService{
         memberPreferList.forEach(memberPrefer -> {memberPrefer.setMember(newMember);});
 
         return memberRepository.save(newMember);
+    }
+
+    @Override
+    public MemberResponseDTO.LoginResultDTO loginMember(MemberRequestDTO.LoginRequestDTO request) {
+        Member member = memberRepository.findByEmail(request.getEmail())
+                .orElseThrow(()-> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        if(!passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+            throw new MemberHandler(ErrorStatus.INVALID_PASSWORD);
+        }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                member.getEmail(), null,
+                Collections.singleton(() -> member.getRole().name())
+        );
+
+        String accessToken = jwtTokenProvider.generateToken(authentication);
+
+        return MemberConverter.toLoginResultDTO(
+                member.getId(),
+                accessToken
+        );
     }
 }
